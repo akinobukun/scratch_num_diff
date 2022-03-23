3732429ee7886050335daeb05b9c04469b8a77f2
Your Name <you@example.com>
2022-03-23 20:08:56 +0900

    update

--- main.py
+++ main.py
@@ -1,55 +1,64 @@
+import glob
 import os
 from flask import Flask, request, redirect, render_template, flash
 from werkzeug.utils import secure_filename
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.preprocessing import image
-
+from src import yolo
 import numpy as np
 
 
-classes = ["0","1","2","3","4","5","6","7","8","9"]
-image_size = 28
-
-UPLOAD_FOLDER = "uploads"
+UPLOAD_FOLDER = "static/cache"
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-model = load_model('./model.h5')#学習済みモデルをロード
+
+def remove_glob(pathname, recursive=True):
+    for p in glob.glob(pathname, recursive=recursive):
+        if os.path.isfile(p):
+            os.remove(p)
+
+
+# 学習済みモデルをロード
+model = yolo.YOLO('model/best.pt')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files:
+        remove_glob(UPLOAD_FOLDER + "/*")
+        # 出力
+        outputs = {}
+        diff = 0
+        if 'files[]' not in request.files:
             flash('ファイルがありません')
             return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('ファイルがありません')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            filepath = os.path.join(UPLOAD_FOLDER, filename)
+        files = request.files.getlist('files[]')
+        for file in files:
+            if file.filename == '':
+                flash('ファイルがありません')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
+                file_path = os.path.join(UPLOAD_FOLDER, filename)
 
-            #受け取った画像を読み込み、np形式に変換
-            img = image.load_img(filepath, grayscale=True, target_size=(image_size,image_size))
-            img = image.img_to_array(img)
-            data = np.array([img])
-            #変換したデータをモデルに渡して予測する
-            result = model.predict(data)[0]
-            predicted = result.argmax()
-            pred_answer = "これは " + classes[predicted] + " です"
+                # 変換したデータをモデルに渡して予測する
+                pred_img, scratch_cnt = model.get_predicted_results(file_path, log=True)
+                pred_answer = str(scratch_cnt)
+                outputs |= {pred_img: pred_answer}
 
-            return render_template("index.html",answer=pred_answer)
+        diff = int(list(outputs.values())[0]) - int(list(outputs.values())[1])
+        return render_template(
+            "index.html", predicted=True, outputs=outputs, diff=np.abs(diff)
+        )
 
-    return render_template("index.html",answer="")
+    return render_template("index.html", predicted=False)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host ='0.0.0.0',port = port)
+    port = 8890
+    app.run(host='0.0.0.0', port=port)
