914d939c5873ccd718d8b15403807b8cf3c00ea5
Your Name <you@example.com>
2022-03-24 21:23:18 +0900

    main fix

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ def upload_file():
     return render_template("index.html", predicted=False)
 
 
+
 if __name__ == "__main__":
-    port = 8890
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ='0.0.0.0',port = port)
